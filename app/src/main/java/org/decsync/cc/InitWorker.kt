192e3d15bcf7029b3798bc87e87162b2b9760297
Aldo Gunsing <dev@aldogunsing.nl>
2020-07-29 20:22:04 +0200

    Use improved CoroutineWorker to show importance to OS

--- app/src/main/java/org/decsync/cc/InitWorker.kt
+++ app/src/main/java/org/decsync/cc/InitWorker.kt
@@ -2,23 +2,23 @@ package org.decsync.cc
 
 import android.content.Context
 import android.os.Build
-import androidx.core.app.NotificationManagerCompat
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
 
 @ExperimentalStdlibApi
-abstract class InitWorker(val context: Context, params: WorkerParameters) : Worker(context, params) {
+abstract class InitWorker(val context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
 
     abstract val type: CollectionInfo.Type
     abstract val authority: String
 
-    override fun doWork(): Result {
+    override suspend fun doWork(): Result {
         val id = inputData.getString(KEY_ID) ?: return Result.failure()
         val name = inputData.getString(KEY_NAME) ?: return Result.failure()
 
         val info = CollectionInfo(type, id, name, context)
 
-        val builder = initSyncNotificationBuilder(context).apply {
+        val notification = initSyncNotificationBuilder(context).apply {
             setSmallIcon(R.drawable.ic_notification)
             setContentTitle(context.getString(
                     when (type) {
@@ -26,32 +26,25 @@ abstract class InitWorker(val context: Context, params: WorkerParameters) : Work
                         CollectionInfo.Type.CALENDAR -> R.string.notification_adding_events
                     },
                     info.name))
-        }
-        with(NotificationManagerCompat.from(context)) {
-            notify(info.notificationId, builder.build())
+        }.build()
+        setForeground(ForegroundInfo(info.notificationId, notification))
+
+        val provider = context.contentResolver.acquireContentProviderClient(authority) ?: return Result.failure()
+        try {
+            val decsync = getDecsync(info)
+            val extra = Extra(info, context, provider)
+            setNumProcessedEntries(extra, 0)
+            decsync.initStoredEntries()
+            decsync.executeStoredEntriesForPathPrefix(listOf("resources"), extra)
+        } finally {
+            if (Build.VERSION.SDK_INT >= 24)
+                provider.close()
+            else
+                @Suppress("DEPRECATION")
+                provider.release()
         }
 
-        val result = context.contentResolver.acquireContentProviderClient(authority)?.let { provider ->
-            try {
-                val decsync = getDecsync(info)
-                val extra = Extra(info, context, provider)
-                setNumProcessedEntries(extra, 0)
-                decsync.initStoredEntries()
-                decsync.executeStoredEntriesForPathPrefix(listOf("resources"), extra)
-                Result.success()
-            } finally {
-                if (Build.VERSION.SDK_INT >= 24)
-                    provider.close()
-                else
-                    @Suppress("DEPRECATION")
-                    provider.release()
-            }
-        } ?: Result.failure()
-
-        with(NotificationManagerCompat.from(context)) {
-            cancel(info.notificationId)
-        }
-        return result
+        return Result.success()
     }
 
     companion object {
